870031e3614553bd60f448cd22bcf4f9e3f2c5b2
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-10-17 01:21:16 +1300

    only pull latest

--- setup-worker.sh
+++ setup-worker.sh
@@ -15,11 +15,11 @@ if [[ $AWS ]]; then
 fi
 
 # use LXC, and disable inter-container communication
-echo 'DOCKER_OPTS="-d --icc=false --exec-driver=lxc '$DOCKER_MOUNT_POINT'"' >> /etc/default/docker
+echo 'DOCKER_OPTS="--icc=false --exec-driver=lxc '$DOCKER_MOUNT_POINT'"' >> /etc/default/docker
 
 service docker restart
 
-docker pull quay.io/travisci/te-worker
+docker pull quay.io/travisci/te-worker:latest
 docker tag quay.io/travisci/te-worker te-worker
 docker pull quay.io/travisci/travis
  
