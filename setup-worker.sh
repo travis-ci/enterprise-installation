7d2cb43ffeaf64e1a73c3d29af4dea61389d2b9a
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-10-17 00:37:16 +1300

    add AWS /mnt support

--- setup-worker.sh
+++ setup-worker.sh
@@ -9,7 +9,16 @@ echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docke
 apt-get update
  
 apt-get install -y linux-image-extra-`uname -r` lxc lxc-docker-1.0.0
- 
+
+if [[ $AWS ]]; then
+  DOCKER_MOUNT_POINT="--graph=/mnt/docker"
+fi
+
+# use LXC, and disable inter-container communication
+echo 'DOCKER_OPTS="-d --exec-driver=lxc --icc=false $DOCKER_MOUNT_POINT"' >> /etc/default/docker
+
+service docker restart
+
 docker pull quay.io/travisci/te-worker
 docker tag quay.io/travisci/te-worker te-worker
 docker pull quay.io/travisci/travis
@@ -21,9 +30,6 @@ done
 # run the install script 
 docker run --rm te-worker cat /usr/local/travis/src/host.sh | bash
 te start
-
-# use LXC, and disable inter-container communication
-echo 'DOCKER_OPTS="-d -e lxc --icc=false"' >> /etc/default/docker
 
 # enable memory and swap accounting (optional, but recommended)
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
